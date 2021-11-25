8147319f0c6b957cf396fc780081e07e90dde2ff
Hanna Vallner <hanna.vallner@gmail.com>
2021-11-25 21:38:07 +0200

    üldised parendused ja js

---
+++ js/script-vta.js
@@ -0,0 +1,15 @@
+function näita_kaarti1(){
+    let image = document.getElementById("kaart1");
+ 
+            image.src="https://i.gyazo.com/6968f94e417503193e87c8028531850e.png"
+            document.getElementById("btnID1")
+            .style.display = "none";
+}
+    
+function näita_kaarti2(){
+    let image = document.getElementById("kaart2");
+ 
+            image.src="https://i.gyazo.com/7ace03bc752c982f77982ba0ab28ecfc.png"
+            document.getElementById("btnID2")
+            .style.display = "none";
+}
