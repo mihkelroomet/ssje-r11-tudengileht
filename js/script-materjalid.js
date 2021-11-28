6a2e5621a8d6667571710300d5f8a984af55c530
Mihkel Roomet <mihkelroomet@gmail.com>
2021-11-28 20:06:21 +0200

    js syntax valideerimiseks

--- js/script-materjalid.js
+++ js/script-materjalid.js
@@ -2,15 +2,14 @@
 //kui algselt on need peidus siis nupuvajutusega teeb nähtavaks ning ka vastupidi
 function tekstivahetus(){
 
-    var nimek = document.getElementById('ullist')
-    var nupp = document.getElementById('nimekiri')
+    var nimek = document.getElementById('ullist');
+    var nupp = document.getElementById('nimekiri');
     if (nimek.style.display == 'none'){
         nimek.style.display = 'block';
-        nupp.innerHTML = "Vajuta siia, et playlistid jälle peitu viia :o"
+        nupp.innerHTML = "Vajuta siia, et playlistid jälle peitu viia :o";
 
     }else{
         nimek.style.display = 'none';
-        nupp.innerHTML = "Vajuta siia, et näha erinevaid playliste"
+        nupp.innerHTML = "Vajuta siia, et näha erinevaid playliste";
     }
 }
-
