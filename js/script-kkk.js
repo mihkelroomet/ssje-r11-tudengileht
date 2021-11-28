5519b46507f36bc0596703a556dc720354cd0f19
mihkelroomet <60692429+mihkelroomet@users.noreply.github.com>
2021-11-28 20:23:03 +0200

    js valideerimiseks

--- js/script-kkk.js
+++ js/script-kkk.js
@@ -3,12 +3,12 @@ var i;
 
 // accordion-elemendile klikkimisel muudetakse küsimuse vastus nähtavaks, kui ta ei ole nähtav ja vastupidi
 for (i = 0; i < ac.length; i++) {
-  ac[i].addEventListener("click", function() {
-    var vastus = this.nextElementSibling;
-    if (vastus.style.display === "block") {
-      vastus.style.display = "none";
-    } else {
-      vastus.style.display = "block";
-    }
-  });
+    ac[i].addEventListener("click", function() {
+        var vastus = this.nextElementSibling;
+        if (vastus.style.display === "block") {
+            vastus.style.display = "none";
+        } else {
+            vastus.style.display = "block";
+        }
+    });
 }
